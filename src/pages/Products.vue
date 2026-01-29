a520ef83974481d30dd63921a25dad1f0e25e0eb
Sabyrka <sabyrka15@gmail.com>
2026-01-30 01:14:47 +0500

    feat: update Products page to display product list with names and prices

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,3 +1,7 @@
+<script setup>
+import { products } from '@/api/mock'
+</script>
+
 <template>
-  <div>Products page</div>
+  <div v-for="product in products" :key="product.id">{{ product.name }} - {{ product.price }}</div>
 </template>
