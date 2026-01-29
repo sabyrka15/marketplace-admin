d2e119e645ffe1c976f037e44282074c14567a6c
Sabyrka <sabyrka15@gmail.com>
2026-01-30 00:06:52 +0500

    feat: add admin routing and menu navigation

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,16 +1,15 @@
 <script setup>
-import { useRouter, useRoute, RouterView } from 'vue-router'
+import { useRoute, RouterView } from 'vue-router'
 
-const router = useRouter()
 const route = useRoute()
 </script>
 
 <template>
   <el-container class="layout">
     <el-aside class="aside">
-      <el-menu :default-active="route.path">
-        <el-menu-item index="/products" @click="router.push('/products')">Products</el-menu-item>
-        <el-menu-item index="/orders" @click="router.push('/orders')">Orders</el-menu-item>
+      <el-menu router :default-active="route.path">
+        <el-menu-item index="/products">Products</el-menu-item>
+        <el-menu-item index="/orders">Orders</el-menu-item>
       </el-menu>
     </el-aside>
 
