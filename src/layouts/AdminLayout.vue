3a7378caec52f9850435716c8ef6c6c0347bcbcf
Sabyrka <sabyrka15@gmail.com>
2026-01-29 23:42:37 +0500

    feat: add admin layout structure

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>Admin layout</div>
+  <el-container class="layout">
+    <el-aside class="aside">Aside</el-aside>
+
+    <el-container>
+      <el-header class="header">Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+
+<style scoped>
+.aside {
+  width: 200px;
+}
+
+.header {
+  height: 60px;
+}
+
+.layout {
+  height: 100vh;
+}
+</style>
